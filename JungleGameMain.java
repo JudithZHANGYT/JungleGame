e96126761f0d57b16f880ea66ccd4ea430626c52
kongyifei0216 <57266002+kongyifei0216@users.noreply.github.com>
2019-11-10 20:47:05 +0800

    Delete JungleGameMain.java

--- JungleGameMain.java
+++
@@ -1,7 +0,0 @@
-//launch the game
-public class JungleGameMain {
-    public static void main(String[] args) {
-        //Record the board including piece's position
-        Piece[][] gameBoard = new Piece[9][7];
-    }
-}
